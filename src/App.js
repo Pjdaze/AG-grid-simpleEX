abbe064bf43a12619d663967910896175a64cf57
Pedro Diaz <pjdux@ilianas-MacBook-Pro.local>
2020-12-07 13:35:11 -0500

    stable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
 
-function App() {
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+
+const App = () => {
+  const [gridApi, setGridApi] = useState(null);
+  const [gridColumnApi, setGridColumnApi] = useState(null);
+  const [ghData, setGhData] = useState({});
+
+  const [rowData, setRowData] = useState([]);
+
+  useEffect(() => {
+    const url = "https://api.github.com/users?since=990";
+    fetch(url)
+      .then((res) => res.json())
+
+      .then((data) => {
+        setRowData([...data]);
+      });
+    console.log(rowData);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+        <AgGridReact rowData={rowData}>
+          <AgGridColumn field="id"></AgGridColumn>
+          <AgGridColumn field="login"></AgGridColumn>
+          <AgGridColumn field="repos_url"></AgGridColumn>
+        </AgGridReact>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
